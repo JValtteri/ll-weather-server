2836f8b1d462a0cdd986b5dd705ef7687d73ed5f
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-02-20 22:18:37 +0200

    Added proxycache

---
+++ proxycache.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    //"fmt"
+    //"encoding/json"
+)
+
+func getCityCoord(city string) float32, float32 {
+    return 61.499, 23.787
+}
+
+func getSummaryWeather(city string) { // JSON
+    lat, lon := getCityCoord(city)
+    // Check cache
+    // Make request
+    // Format response
+    // Cache response
+    // Return responce
+}
+
+func getDetailWeather(city string) { // JSON
+    lat, lon := getCityCoord(city)
+    // Check cache
+    // Make request
+    // Format response
+    // Cache response
+    // Return responce
+}
